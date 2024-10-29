16c5376ae19a433189d90a459bc21422ef2231b2
Md. Tahmid Rahman <157820278+mdtahmidrahman@users.noreply.github.com>
2024-10-30 03:24:14 +0600

    Update fracKnap_ratio.cpp

--- fracKnap_ratio.cpp
+++ fracKnap_ratio.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n, maxW;
@@ -14,6 +15,7 @@ int main()
         cin >> weight[i] >> price[i];
         ratio[i] = price[i] / weight[i];
     }
+
     for(int i = 0; i < n - 1; i++)
     {
         for(int j = i + 1; j < n; j++)
@@ -26,23 +28,28 @@ int main()
             }
         }
     }
+
     double totalValue = 0.0;
     int currentWeight = 0;
 
+    cout << "Selected items:\n";
     for(int i = 0; i < n; i++)
     {
         if(currentWeight + weight[i] <= maxW)
         {
             currentWeight += weight[i];
             totalValue += price[i];
+            cout << "Item " << i << " fully taken, Weight: " << weight[i] << ", Price: " << price[i] << "\n";
         }
         else
         {
             int remainingWeight = maxW - currentWeight;
             totalValue += ratio[i] * remainingWeight;
+            cout << "Item " << i << " partially taken, Weight: " << remainingWeight << ", Price: " << ratio[i] * remainingWeight << "\n";
             break;
         }
     }
+
     cout << "Total value in knapsack: " << totalValue << endl;
     return 0;
 }
