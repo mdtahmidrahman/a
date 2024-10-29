06760b434ed33ed09ba7b2548230d20f1f77d22b
Md. Tahmid Rahman <157820278+mdtahmidrahman@users.noreply.github.com>
2024-10-30 02:04:18 +0600

    Create fracKnap_ratio.cpp

---
+++ fracKnap_ratio.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, maxW;
+    cout << "Max W: "; cin >> maxW;
+    cout << "Items: "; cin >> n;
+
+    double weight[n], price[n], ratio[n];
+    cout << "Weight & price:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Item " << i << ": ";
+        cin >> weight[i] >> price[i];
+        ratio[i] = price[i] / weight[i];
+    }
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(ratio[i] < ratio[j])
+            {
+                swap(ratio[i], ratio[j]);
+                swap(weight[i], weight[j]);
+                swap(price[i], price[j]);
+            }
+        }
+    }
+    double totalValue = 0.0;
+    int currentWeight = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(currentWeight + weight[i] <= maxW)
+        {
+            currentWeight += weight[i];
+            totalValue += price[i];
+        }
+        else
+        {
+            int remainingWeight = maxW - currentWeight;
+            totalValue += ratio[i] * remainingWeight;
+            break;
+        }
+    }
+    cout << "Total value in knapsack: " << totalValue << endl;
+    return 0;
+}
